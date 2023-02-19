bfe30519262dd3c974f284d3d4757096fd8dfa6e
Raul Diamantino <rauldiamantino25@gmail.com>
2023-02-19 19:45:29 -0300

    Feat: Adicionei includes php para tornar o topo e rodapé dinâmicos.

---
+++ src/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./assets/style.css">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <title>Loja Virtual</title>
+</head>
+
+<body>
+  <?php include('./partials/topo.php'); ?>
+  
+  <main class="h-screen w-full bg-neutral-50 css-conteudo-principal">
+  </main>
+
+  <?php include('./partials/rodape.php'); ?>
+
+  <script type="module" src="./assets/js/script.js"></script>
+</body>
+
+</html>
--- src/partials/topo.php
+++ src/partials/topo.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./assets/style.css">
-  <script src="https://cdn.tailwindcss.com"></script>
-  <title>Loja Virtual</title>
-</head>
-
-<body>
-  <header class="sticky top-0 z-50 bg-white css-topo">
+<header class="sticky top-0 z-50 bg-white css-topo">
     <section class="w-full h-9 flex justify-center items-center bg-neutral-800 css-topo-aviso">
       <p class="text-white text-sm css-topo-aviso-texto">PROJETO EM DESENVOLVIMENTO</p>
     </section>
@@ -382,60 +369,3 @@
     </section>
     <section class="lg:hidden w-full h-screen hidden fixed top-0 left-0 z-40 bg-black/80 css-area-externa"></section>
   </header>
-
-  <main class="h-screen w-full bg-neutral-50 css-conteudo-principal">
-  </main>
-
-  <footer class="border-t border-neutral-200 h-max absolute top-full bottom-0 left-0 right-0 py-10 rounded-t-3xl bg-white css-rodape">
-    <section class="lg:mx-auto lg:w-4/6 lg:p-0 lg:grid-cols-3 lg:justify-start lg:text-base md:grid md:grid-cols-2 px-4 flex flex-col gap-4 text-sm bg-white css-rodape-superior">
-      <section class="bg-white w-full css-rodape-lista">
-        <ul>
-          <h2 class="py-2 font-normal">Acesso rápido</h2>
-          <li class="lg:leading-6 lg:text-sm text-xs text-neutral-500 leading-loose"><a href="">Como funciona</a></li>
-          <li class="lg:leading-6 lg:text-sm text-xs text-neutral-500 leading-loose"><a href="">Sua conta</a></li>
-          <li class="lg:leading-6 lg:text-sm text-xs text-neutral-500 leading-loose"><a href="">Site Institucional</a></li>
-          <li class="lg:leading-6 lg:text-sm text-xs text-neutral-500 leading-loose"><a href="">Termos de uso</a></li>
-          <li class="lg:leading-6 lg:text-sm text-xs text-neutral-500 leading-loose"><a href="">Declaração de Privacidade</a></li>
-          <li class="lg:leading-6 lg:text-sm text-xs text-neutral-500 leading-loose"><a href="">Como Comprar</a></li>
-          <li class="lg:leading-6 lg:text-sm text-xs text-neutral-500 leading-loose"><a href="">Politica Global Lgpd</a></li>
-          <li class="lg:leading-6 lg:text-sm text-xs text-neutral-500 leading-loose"><a href="">Trocas</a></li>
-        </ul>        
-      </section>
-
-      <section class="lg:col-start-2 md:col-start-1 bg-white w-full css-rodape-pagamentos">
-        <h2 class="pb-2 font-normal">Formas de pagamento</h2>
-        <img src="./assets/images/pagamento.png" class="lg:w-52 pt-1 w-48">
-      </section>
-
-      <section class="lg:col-start-3 md:row-start-1 md:col-start-2 bg-white w-full css-rodape-textos">
-        <h2 class="py-2 font-normal">Fale conosco</h2>
-        <p class="lg:leading-6 lg:text-sm flex justify-start items-center gap-2 text-xs text-neutral-500 leading-loose">
-          <span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="opacity-50" viewBox="0 0 16 16">
-              <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
-            </svg>
-          </span> (11) 90000-0000
-        </p>
-        <p class="lg:leading-6 lg:text-sm flex justify-start items-center gap-2 text-xs text-neutral-500 leading-loose">
-          <span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="opacity-50" viewBox="0 0 16 16">
-              <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
-            </svg>
-          </span> email@email.com
-        </p>
-        <p class="lg:leading-6 lg:text-sm text-xs text-neutral-500 leading-loose">De Segunda à Sábado das 08h às 19h</p>
-
-        <h2 class="mt-3 py-2 font-normal">Loja Virtual</h2>
-        <p class="lg:leading-6 lg:text-sm text-xs text-neutral-500 leading-loose">Av. Pedro Álvares Cabral, 1000<br> Ibirapuera, São Paulo-SP, 00000-000</p>
-        <p class="lg:leading-6 lg:text-sm text-xs text-neutral-500 leading-loose">Empresa Fictícia de Comércio Eletrônico</p>        
-      </section>
-      <section>
-        <p class="text-sm font-bold">&copy 2023 Raul Diamantino</p>
-      </section>
-    </section>
-  </footer>
-
-  <script type="module" src="./assets/js/script.js"></script>
-</body>
-
-</html>
