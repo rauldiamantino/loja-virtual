911bdbdf83390b46b148dd550f7a0283d32fbc55
Raul Diamantino <rauldiamantino25@gmail.com>
2023-02-20 00:31:35 -0300

    Feat: Função para rotacionar imagens do banner principal

--- src/assets/js/components/banner-principal.js
+++ src/assets/js/components/banner-principal.js
@@ -1,15 +1,42 @@
 const bannerPrincipal = ($alvo) => {
     const $btnLeft = document.querySelector('.css-banner-principal-btn-left');
     const $btnRight = document.querySelector('.css-banner-principal-btn-right');
-    const $carrosselClicado = $alvo.target.closest('.css-banner-principal');
     const $clicouBtnLeft = $alvo.target == $btnLeft;
     const $clicouBtnRight = $alvo.target == $btnRight;
+    const $carrosselClicado = $alvo.target.closest('.css-banner-principal');
+    const $imagens = $carrosselClicado.querySelector('.css-banner-principal-imagens');
+    const $qtdeImagens = $imagens.querySelectorAll('img');
+    let contador = $imagens.id;
     if ($clicouBtnLeft) {
-        console.log($carrosselClicado);
+        diminuirContador(contador, $imagens);
+        $imagens.style.transform = `translateX(${contador * -100}%)`;
     }
     if ($clicouBtnRight) {
-        console.log($carrosselClicado);
+        aumentarContador(contador, $imagens);
+        $imagens.style.transform = `translateX(${contador * -100}%)`;
     }
+};
+const aumentarContador = (contador, $imagens) => {
+    const $contadorMaximo = $imagens.querySelectorAll('img').length;
+    contador = $imagens.id;
+    if (contador >= $contadorMaximo - 1) {
+        contador = 0;
+    }
+    else {
+        contador++;
+    }
+    $imagens.id = contador;
+};
+const diminuirContador = (contador, $imagens) => {
+    const $contadorMaximo = $imagens.querySelectorAll('img').length;
+    contador = $imagens.id;
+    if (contador <= 0) {
+        contador = $contadorMaximo - 1;
+    }
+    else {
+        contador--;
+    }
+    $imagens.id = contador;
 };
 export { bannerPrincipal };
 //# sourceMappingURL=banner-principal.js.map
--- src/assets/ts/components/banner-principal.ts
+++ src/assets/ts/components/banner-principal.ts
@@ -1,17 +1,50 @@
 const bannerPrincipal = ($alvo:any) => {
   const $btnLeft:HTMLButtonElement = document.querySelector('.css-banner-principal-btn-left')
   const $btnRight:HTMLButtonElement = document.querySelector('.css-banner-principal-btn-right')
-  const $carrosselClicado:HTMLElement = $alvo.target.closest('.css-banner-principal')
   const $clicouBtnLeft:Boolean = $alvo.target == $btnLeft
-  const $clicouBtnRight:Boolean = $alvo.target == $btnRight    
+  const $clicouBtnRight:Boolean = $alvo.target == $btnRight
+  const $carrosselClicado:HTMLElement = $alvo.target.closest('.css-banner-principal')
+  const $imagens:HTMLElement = $carrosselClicado.querySelector('.css-banner-principal-imagens')  
+  const $qtdeImagens:NodeList = $imagens.querySelectorAll('img')
+  let contador:any = $imagens.id
 
   if($clicouBtnLeft) {
-    console.log($carrosselClicado)
+    diminuirContador(contador, $imagens)
+    $imagens.style.transform = `translateX(${contador * -100}%)`
   }
   
-  if($clicouBtnRight) {    
-    console.log($carrosselClicado)
+  if($clicouBtnRight) {
+    aumentarContador(contador, $imagens)
+    $imagens.style.transform = `translateX(${contador * -100}%)`
   }
+}
+
+const aumentarContador = (contador:number, $imagens:any) => {
+  const $contadorMaximo:number = $imagens.querySelectorAll('img').length
+
+  contador = $imagens.id
+
+  if(contador >= $contadorMaximo - 1) {
+    contador = 0
+  } else {
+    contador++
+  }
+
+  $imagens.id = contador
+}
+
+const diminuirContador = (contador:number, $imagens:any) => {
+  const $contadorMaximo:number = $imagens.querySelectorAll('img').length
+  
+  contador = $imagens.id
+  
+  if(contador <= 0) {
+    contador = $contadorMaximo - 1
+  } else {
+    contador--
+  }
+
+  $imagens.id = contador
 }
 
 export { bannerPrincipal }
