54cf3eca9f91b74d91317d32951b7f57b9559a48
Raul Diamantino <rauldiamantino25@gmail.com>
2023-02-19 21:00:40 -0300

    Feat: Criei arquivo head.php e chamei dinamicamente no index.

--- src/index.php
+++ src/index.php
@@ -1,24 +1,16 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./assets/style.css">
-  <script src="https://cdn.tailwindcss.com"></script>
-  <title>Loja Virtual</title>
-</head>
-
-<body>
-  <?php include('./partials/topo.php'); ?>
+  <?php include("./partials/head.php"); ?>
   
-  <main class="h-screen w-full bg-neutral-50 css-conteudo-principal">
-  </main>
+  <body>    
+    <?php include("./partials/header.php"); ?>
 
-  <?php include('./partials/rodape.php'); ?>
-
-  <script type="module" src="./assets/js/script.js"></script>
-</body>
+    <main class="h-screen w-full bg-neutral-50 css-conteudo-principal">
+    </main>
+    
+    <?php include("./partials/footer.php"); ?>
+    
+    <script type="module" src="./assets/js/script.js"></script>
+  </body>
 
 </html>
---
+++ src/partials/head.php
@@ -0,0 +1,9 @@
+<?php ?>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./assets/style.css">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <title>Loja Virtual</title>
+</head>
