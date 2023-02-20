2ac91637098e3a5f6bb39acc6b2b160995e52a1b
Raul Diamantino <rauldiamantino25@gmail.com>
2023-02-20 19:07:08 -0300

    Refactor: Ajuste do código index.php e remoção de espaços.

--- src/assets/ts/script.ts
+++ src/assets/ts/script.ts
@@ -14,5 +14,5 @@ const navegadorRedimensionado = () => {
 }
 
 elementoClicado()
-navegadorRedimensionado()
 bannerPrincipal()
+navegadorRedimensionado()
--- src/index.php
+++ src/index.php
@@ -1,22 +1,26 @@
 <!DOCTYPE html>
 <html lang="pt-br">
   <?php include("./partials/head.php"); ?>
-  
-  <body>    
+
+  <body>
     <?php include("./partials/header.php"); ?>
 
     <main class="h-full w-full bg-neutral-900 css-conteudo-principal">     
       <?php 
         $banner = [
-          "imagens" => ['./assets/images/banner-principal/1.jpg', './assets/images/banner-principal/2.jpg', './assets/images/banner-principal/3.jpg'],
+          "imagens" => [
+            './assets/images/banner-principal/1.jpg',
+            './assets/images/banner-principal/2.jpg',
+            './assets/images/banner-principal/3.jpg'
+          ],
         ];
-        
+
         include("./partials/banner-principal.php"); 
       ?>
     </main>
-    
+
     <?php include("./partials/footer.php"); ?>
-    
+
     <script type="module" src="./assets/js/script.js"></script>
   </body>
 
