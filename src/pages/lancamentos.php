14dcf7dcc564ea35ecfba8e8f55af4f0d63222eb
Raul Diamantino <rauldiamantino25@gmail.com>
2023-03-03 09:48:09 -0300

    Feat: Adicionei demais dados do produto na listagem

--- src/pages/lancamentos.php
+++ src/pages/lancamentos.php
@@ -7,18 +7,16 @@
         "codigo" => 001,
         "referencia" => "rd001",
         "nome" => "Tênis Nike Revolution 6 - Masculino",
-        "preco" => 299.99,
+        "preco-de" => 459.99,
+        "preco-por" => 399.99,
+        "qtde-parcelas" => 3,
         "variavel-1" => [
           "cor-1" => [
-            "variavel-2" => [39, 40, 41, 42, 43, 44],
+            "variavel-2" => [39, 40, 41],
             "imagens" => [
-              "1" => "../assets/images/produtos/001/1.webp",
-              "2" => "../assets/images/produtos/001/2.webp",
-              "3" => "../assets/images/produtos/001/3.webp",
-              "4" => "../assets/images/produtos/001/4.webp",
-              "5" => "../assets/images/produtos/001/5.jpg",
-              "6" => "../assets/images/produtos/001/6.webp",
-              "7" => "../assets/images/produtos/001/7.jpg"
+              "../assets/images/produtos/001/1.webp",
+              "../assets/images/produtos/001/2.webp",
+              "../assets/images/produtos/001/3.webp",
             ],
           ],
         ],
@@ -37,23 +35,21 @@
         "codigo" => 033,
         "referencia" => "rd033",
         "nome" => "Tênis Nike - Feminino",
-        "preco" => 159.99,
+        "preco-de" => 659.99,
+        "preco-por" => 299.99,
+        "qtde-parcelas" => 6,
         "variavel-1" => [
           "cor-1" => [
-            "variavel-2" => [39, 40, 41, 42, 43, 44],
+            "variavel-2" => [42, 43, 44],
             "imagens" => [
-              "1" => "../assets/images/produtos/001/6.webp",
-              "2" => "../assets/images/produtos/001/2.webp",
-              "3" => "../assets/images/produtos/001/3.webp",
-              "4" => "../assets/images/produtos/001/4.webp",
-              "5" => "../assets/images/produtos/001/5.jpg",
-              "6" => "../assets/images/produtos/001/6.webp",
-              "7" => "../assets/images/produtos/001/7.jpg"
+              "../assets/images/produtos/001/5.jpg",
+              "../assets/images/produtos/001/6.webp",
+              "../assets/images/produtos/001/7.jpg"
             ],
           ],
         ],
         "descricao" => [
-          "Seu companheiro em sua prática esportiva ou dia a dia é esse tênis Nike masculino é a opção perfeita. O cabedal (parte superior do calçado) em malha respirável proporciona suporte, apoio e durabilidade. A entressola em EVA e solado emborrachado que garantem maior estabilidade e aderência em todas as superfícies. O tênis conta também contraforte acolchoado para maior suporte, ficando livre de atrito. Além disso, seu design super moderno e o padrão Nike em todos os detalhes do calçado. Confira e compre hoje!"
+          "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam quo unde magni aspernatur sunt incidunt tempora. Magnam molestiae dolorum, impedit distinctio cupiditate autem obcaecati ad alias fuga, commodi, in odit?"
         ],
       ];
       include('./partials/produto-listagem.php');
--- src/partials/produto-listagem.php
+++ src/partials/produto-listagem.php
@@ -1,7 +1,7 @@
 <section class="px-2 border border-transparent hover:border-gray-200 css-produto-listagem">
   <div class="css-produto-imagem-principal">
     <img src=
-      <?php print_r($produto["variavel-1"]["cor-1"]["imagens"]["1"]) ?>
+      <?php print_r($produto["variavel-1"]["cor-1"]["imagens"][0]) ?>
     >
   </div>
   <div class="p-4 css-produto-textos">
@@ -10,13 +10,17 @@
     </h2>
     <div class="pt-2 text-sm css-pt-precos">
       <h4 class="text-gray-400 line-through leading-tight css-ptp-preco-de">
-        R$ 459,90
+        R$ <?php print_r($produto["preco-de"]) ?>
       </h4>
       <h3 class="leading-tight text-lg font-medium css-ptp-preco-por">
-        R$ <?php print_r($produto["preco"]) ?>
+        R$ <?php print_r($produto["preco-por"]) ?>
       </h3>
       <h4 class="leading-tight css-ptp-parcelas">
-        ou 2x de R$ 149,99
+        ou <?php print_r($produto["qtde-parcelas"]) ?>x de R$ <?php
+             $total = $produto["preco-por"] / $produto["qtde-parcelas"];
+             $totalFormatado = number_format($total, 2, ",", ".");
+             print_r($totalFormatado);
+           ?>
       </h4>
     </div>
   </div>
