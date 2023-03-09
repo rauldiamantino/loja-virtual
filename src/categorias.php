0800e2d1aff9101b78c265231bff5a0d933b5783
Raul Diamantino <rauldiamantino25@gmail.com>
2023-03-08 21:31:36 -0300

    Refactor: Alterei o código para PHP e chamei o HTML via echo

--- src/categorias.php
+++ src/categorias.php
@@ -1,14 +1,22 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<?php include("./partials/head.php"); ?>
+<?php
+  echo "<!DOCTYPE html>
+        <html lang='pt-br'>";
 
-<body>
-  <?php include("./partials/header.php"); ?>
-  <main class="xl:mx-auto xl:max-w-7xl py-2">
-    <?php include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php"); ?>
-  </main>
-  <?php include("./partials/footer.php"); ?>
+  include("./partials/head.php");
 
-  <script type="module" src="./assets/js/script.js"></script>
-</body>
-</html>
+  echo "<body>";
+  
+  include("./partials/header.php");
+  
+  echo "<main class='xl:mx-auto xl:max-w-7xl py-2'>";
+
+  include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php");
+
+  echo "</main>";
+
+  include("./partials/footer.php");
+
+  echo "<script type='module' src='./assets/js/script.js'></script>
+      </body>
+    </html>";
+?>
